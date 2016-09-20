bad4d7ef5f27745d822591bd1df0acbec9eaa697
myLoveMusic <1991770693@qq.com>
2016-09-20 08:57:43 +0800

    添加监听

--- app/src/main/java/com/lihang/frescodemo/MainActivity.java
+++ app/src/main/java/com/lihang/frescodemo/MainActivity.java
@@ -1,13 +1,21 @@
 package com.lihang.frescodemo;
 
+import android.graphics.drawable.Animatable;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.common.logging.FLog;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.image.QualityInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.lidroid.xutils.ViewUtils;
@@ -24,24 +32,76 @@ public class MainActivity extends AppCompatActivity {
     private String img_url3 = "http://hiphotos.baidu.com/%CC%EC%C9%BD%B6%FE%CF%C0%B5%C4%D0%A1%CE%DD/pic/item/70c553e736d12f2e5b0614d64fc2d5628535682a.jpg";
     private String img_url4 = "http://img4.douban.com/view/photo/raw/public/p1773847919.jpg";
     private String img_url5 = "http://7mno4h.com2.z0.glb.qiniucdn.com/3859deb07d4647cf9183f8ea3f5aa165.jpg";//大图
-    private String img_url6 = img_url5+"/test";//小图
+    private String img_url6 = img_url5 + "/test";//小图
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewUtils.inject(this);
-        requestImage();
 //        Uri uri = Uri.parse(img_url0);
 //        simpleDraweeView.setImageURI(uri);
+//        requestImage();
+        moreImages();
+
+
     }
 
-    private void requestImage(){
-        ImageRequest request = ImageRequestBuilder.newBuilderWithSource( Uri.parse(img_url4))
+    private void requestImage() {
+        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(img_url4))
                 .setProgressiveRenderingEnabled(true)
                 .build();
-            PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+        PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
                 .setImageRequest(request)
+                .setOldController(simpleDraweeView.getController())
+                .build();
+        simpleDraweeView.setController(controller);
+    }
+
+    private void moreImages() {
+        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+            //成功
+            @Override
+            public void onFinalImageSet(
+                    String id,
+                    @Nullable ImageInfo imageInfo,
+                    @Nullable Animatable anim) {
+                if (imageInfo == null) {
+                    return;
+                }
+                QualityInfo qualityInfo = imageInfo.getQualityInfo();
+                FLog.d("Final image received! " +
+                                "Size %d x %d",
+                        "Quality level %d, good enough: %s, full quality: %s",
+                        imageInfo.getWidth(),
+                        imageInfo.getHeight(),
+                        qualityInfo.getQuality(),
+                        qualityInfo.isOfGoodEnoughQuality(),
+                        qualityInfo.isOfFullQuality());
+                Log.e("==========>>","Final image received! " +
+                                "Size "+imageInfo.getWidth()+" x "+imageInfo.getHeight()+
+                        "Quality level "+qualityInfo.getQuality()+", good enough: "+qualityInfo.isOfGoodEnoughQuality()+", full quality: "+qualityInfo.isOfFullQuality());
+            }
+
+            //渐进
+            @Override
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+                FLog.d("", "Intermediate image received");
+                Log.e("=========>>","Intermediate image received");
+            }
+
+            //失败
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                FLog.e(getClass(), throwable, "Error loading %s", id);
+                Log.e("", "Error loading "+id);
+            }
+        };
+
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setControllerListener(controllerListener)
+                .setLowResImageRequest(ImageRequest.fromUri(img_url6))
+                .setImageRequest(ImageRequest.fromUri(img_url5))
                 .setOldController(simpleDraweeView.getController())
                 .build();
         simpleDraweeView.setController(controller);
